32e57a75728ee331b0a9aee4f2cc49ff92311f18
UmmIt <git@ummit.dev>
2024-12-30 06:36:04 +0800

    test: update Docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'gpu-passthru.ummit.dev',
+  url: 'https://gpu-passthru.ummit.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
